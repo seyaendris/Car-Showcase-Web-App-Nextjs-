dc18b25fd5aa5254721693d5d6f66b7d11ce272c
seyaendris <seide9894@gmail.com>
2024-12-18 16:10:05 +0300

    created a heading and subtitle

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      Hero Section
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'>
+        <h1 className='hero__title'>
+            Find, Book, or Rent a car -- quickly and easily!
+        </h1>
+        <p className='hero_subtitle'>
+            Streamline your car rental experience with our effortless booking process.
+        </p>
+
+      </div>
     </div>
   )
 }
