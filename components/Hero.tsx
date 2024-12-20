787c90bd2c16c54b5224dfe7cbdb22e4b794468b
seyaendris <seide9894@gmail.com>
2024-12-20 14:31:38 +0300

    added a button to the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,13 @@
+"use client"
+
 import React from 'react'
 import CustomButton from './CustomButton'
 
 const Hero = () => {
+  const handleScroll = () => {
+
+  }
+
   return (
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x'>
@@ -12,7 +18,11 @@ const Hero = () => {
             Streamline your car rental experience with our effortless booking process.
         </p>
 
-        <CustomButton />
+        <CustomButton 
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white font-semibold rounded-full mt-10"
+          handleClick={handleScroll}
+          />
 
       </div>
     </div>
