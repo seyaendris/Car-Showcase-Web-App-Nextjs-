11c5a67af6af5a59f8cd25dff8585cca460047b6
seyaendris <seide9894@gmail.com>
2024-12-24 21:31:50 +0300

    optionProps added

--- types/index.ts
+++ types/index.ts
@@ -15,6 +15,11 @@ export interface CustomButtonProps {
     setManufacturer: (manufacturer: string) => void;
   }
 
+  export interface OptionProps {
+    title: string;
+    value: string;
+  }
+
   export interface CustomFilterProps {
     title: string;
     options: OptionProps[];
