227923fe74619c5a2fa2d2df4872ed127309fbfd
seyaendris <seide9894@gmail.com>
2024-12-29 15:38:55 +0300

    created a GET request handler

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,16 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
+
+
+export const fetchCars = async () => {
+	const headers = {
 		'x-rapidapi-key': '9803fff763msh838d46198e892c6p1b8465jsncb058bd6784a',
 		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
-};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+	const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+		headers: headers
+	}) ;
+
+	const result = await response.json()
+	return result
+
 }
