199a7f2c74c7c4242a150c97125951bdbdf02bae
seyaendris <seide9894@gmail.com>
2024-12-29 15:30:43 +0300

    updated

--- utils/index.ts
+++ utils/index.ts
@@ -1,41 +1,16 @@
-
-export const calculateCarRent = (city_mpg: number, year: number) => {
-  const basePricePerDay = 50; // Base rental price per day in dollars
-  const mileageFactor = 0.1; // Additional rate per mile driven
-  const ageFactor = 0.05; // Additional rate per year of vehicle age
-
-  // Calculate additional rate based on mileage and age
-  const mileageRate = city_mpg * mileageFactor;
-  const ageRate = (new Date().getFullYear() - year) * ageFactor;
-
-  // Calculate total rental rate per day
-  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
-
-  return rentalRatePerDay.toFixed(0);
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '9803fff763msh838d46198e892c6p1b8465jsncb058bd6784a',
+		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
 };
 
-export const updateSearchParams = (type: string, value: string) => {
-	// Get the current URL search params
-	const searchParams = new URLSearchParams(window.location.search);
-  
-	// Set the specified search parameter to the given value
-	searchParams.set(type, value);
-  
-	// Set the specified search parameter to the given value
-	const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
-  
-	return newPathname;
-  };
-
-  export const deleteSearchParams = (type: string) => {
-	// Set the specified search parameter to the given value
-	const newSearchParams = new URLSearchParams(window.location.search);
-  
-	// Delete the specified search parameter
-	newSearchParams.delete(type.toLocaleLowerCase());
-  
-	// Construct the updated URL pathname with the deleted search parameter
-	const newPathname = `${window.location.pathname}?${newSearchParams.toString()}`;
-  
-	return newPathname;
-  };
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
