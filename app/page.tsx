6f34bc5c82e4447523da8bec3a4739f9bdbdce41
seyaendris <seide9894@gmail.com>
2024-12-29 16:55:52 +0300

    interface created

--- app/page.tsx
+++ app/page.tsx
@@ -29,10 +29,10 @@ export default async function Home() {
 
         { !isDataEmpty ? (
           <section>
-            <div className="home__cars-wrapper">
-              { allCars.map((car) => (
-                <CarCard car={ car } />
-              ) ) }
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
             </div>
           </section>
         ): (
