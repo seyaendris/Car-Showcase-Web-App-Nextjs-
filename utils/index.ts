69476ef3230f756a73d5b8b83f5b93c8694e3396
seyaendris <seide9894@gmail.com>
2024-12-29 16:08:32 +0300

    put it in try catch block

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,22 @@
-
-
 export const fetchCars = async () => {
-	const headers = {
-		'x-rapidapi-key': '9803fff763msh838d46198e892c6p1b8465jsncb058bd6784a',
-		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+	try {
+		const headers = {
+			'x-rapidapi-key': '9803fff763msh838d46198e892c6p1b8465jsncb058bd6784a',
+			'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+		};
+
+		const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+			headers: headers
+		});
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+
+		const result = await response.json();
+		return result;
+	} catch (error: any) {
+		console.error('Error fetching cars:', error.message);
+		throw error;
 	}
-
-	const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
-		headers: headers
-	}) ;
-
-	const result = await response.json()
-	return result
-
-}
+};
