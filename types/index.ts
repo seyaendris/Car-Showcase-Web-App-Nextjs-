b025d72ea4b861764e56bba59b8e3a50643b8e43
seyaendris <seide9894@gmail.com>
2024-12-29 16:53:40 +0300

    CarCard props created

--- types/index.ts
+++ types/index.ts
@@ -24,3 +24,18 @@ export interface CustomButtonProps {
     title: string;
     options?: OptionProps[];
   }
+
+  export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+  }
