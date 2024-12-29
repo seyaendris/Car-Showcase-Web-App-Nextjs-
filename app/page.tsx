461122b25a63d77359d172a75480e57dbcf7633d
seyaendris <seide9894@gmail.com>
2024-12-29 16:10:12 +0300

    conditionally rendered

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 export default async function Home() {
   const allCars = await fetchCars()
   console.log(allCars)
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -23,8 +25,19 @@ export default async function Home() {
             <CustomFilter title="fuel" />
             <CustomFilter title="year" />
           </div>
-
         </div>
+
+        { !isDataEmpty ? (
+          <section>
+            WE HAVE CARS
+          </section>
+        ): (
+          <div className="home__error-container">
+            <h2 className="text-xl text-black font-bold">opps! no result</h2>
+            <p>{ allCars.message }</p>
+          </div>
+        ) }
+
       </div>
 
     </main>
