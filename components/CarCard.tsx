da5e8dde69e230eab3710e99875e94a4bf26b481
seyaendris <seide9894@gmail.com>
2024-12-29 17:03:01 +0300

    updated

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -8,9 +8,21 @@ interface CarCardProps {
   }
 
 const CarCard = ({ car }: CarCardProps) => {
+    const { city_mpg, year, make, model, transmission, drive } = car;
+
   return (
-    <div>
-      
+    <div className='car-card group'>
+      <div className='car-card__content'>
+        <h2 className='car-card__content-title'>
+            {make} {model}
+        </h2>
+      </div>
+
+      <p>
+        <span>
+            Car Rent ...
+        </span>
+      </p>
     </div>
   )
 }
