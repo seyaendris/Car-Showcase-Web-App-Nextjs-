e4aae778b659199973ee987c7919969c1f1374d1
seyaendris <seide9894@gmail.com>
2024-12-30 16:32:24 +0300

    updated the fetch request

--- utils/index.ts
+++ utils/index.ts
@@ -20,7 +20,7 @@ export const fetchCars = async () => {
 			'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
 		};
 
-		const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+		const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
 			headers: headers
 		});
 
