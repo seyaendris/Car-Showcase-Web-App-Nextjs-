4013268a01b94e1bee3898dde4e619b596c303e8
seyaendris <seide9894@gmail.com>
2024-12-30 17:04:52 +0300

    updated

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { CarProps } from '@/types'
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import Image from 'next/image';
 import React, { useState } from 'react'
 import CustomButton from './CustomButton';
@@ -30,7 +30,7 @@ const CarCard = ({ car }: CarCardProps) => {
       </p>
       <div className='relative w-full h-40 my-3 object-contain'>
         <Image 
-            src="/hero.png"
+            src={generateCarImageUrl(car)}
             alt='car model'
             fill
             priority
