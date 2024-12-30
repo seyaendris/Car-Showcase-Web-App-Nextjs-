c4ce7f4310af6fc4e9483c7faed765bb7b1dedcb
seyaendris <seide9894@gmail.com>
2024-12-30 09:29:00 +0300

    custom button added

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,6 +4,7 @@ import { CarProps } from '@/types'
 import { calculateCarRent } from '@/utils';
 import Image from 'next/image';
 import React from 'react'
+import CustomButton from './CustomButton';
 
 interface CarCardProps {
     car: CarProps;
@@ -72,6 +73,16 @@ const CarCard = ({ car }: CarCardProps) => {
                 { city_mpg } MPG
               </p>
           </div>
+        </div>
+        <div className="car-card__btn-container">
+
+          <CustomButton
+            title='View More'
+            containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+            textStyles='text-white text-[14px] leading-[17px] font-bold'
+            rightIcon='/right-arrow.svg'
+            handleClick={() => setIsOpen(true)}
+          />
         </div>
         
       </div>
