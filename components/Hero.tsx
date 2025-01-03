3f11bc28fa52244e062008ba5c53771965eb915b
seyaendris <seide9894@gmail.com>
2025-01-03 23:04:57 +0300

    hero updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
     <div className='hero'>
       <div className='flex-1 pt-20 padding-x'>
         <h1 className='hero__title'>
-            Find, Book, or Rent a car -- quickly and easily!
+            {/* Find, Book, or Rent a car -- quickly and easily! */}
         </h1>
         <p className='hero_subtitle'>
             Streamline your car rental experience with our effortless booking process.
