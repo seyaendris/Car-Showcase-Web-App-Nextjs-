a663c32933042d348c1474785cffaae519c3a365
seyaendris <seide9894@gmail.com>
2025-01-03 23:03:40 +0300

    import removed

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -6,8 +6,6 @@ import { Combobox, Transition } from "@headlessui/react";
 import { manufacturers } from "@/constants";
 import { SearchManuFacturerProps } from "@/types";
 
-// import { manufacturers } from "@constants";
-// import { SearchManuFacturerProps } from "@types";
 
 const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManuFacturerProps) => {
   const [query, setQuery] = useState("");
