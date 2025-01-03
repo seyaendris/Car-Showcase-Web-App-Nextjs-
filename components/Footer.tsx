e99e0f506e0069b62e7bda8d3a10e01ef905ed1e
seyaendris <seide9894@gmail.com>
2025-01-03 23:08:21 +0300

    footer copyright removed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -38,14 +38,14 @@ const Footer = () => (
     <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
       <p>@2024 CarHub. All rights reserved</p>
 
-      <div className="footer__copyrights-link">
+      {/* <div className="footer__copyrights-link">
         <Link href="/" className="text-gray-500">
           Privacy & Policy
         </Link>
         <Link href="/" className="text-gray-500">
           Terms & Condition
         </Link>
-      </div>
+      </div> */}
     </div>
   </footer>
 );
